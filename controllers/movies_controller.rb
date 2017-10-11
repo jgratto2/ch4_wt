50c5ce7d293f27fa28d1cad753a3ec726be4103a
jgratto2 <jgratto2@uncc.edu>
2017-10-11 06:38:04 +0000

    Done with section 4.2.

--- controllers/movies_controller.rb
+++ controllers/movies_controller.rb
@@ -11,7 +11,13 @@ def edit
   @movie = Movie.find params[:id]
 end
 
-
+def update
+    @movie = Movie.find params[:id]
+    #@movie.update_attributes!(params[:movie])  # old way
+    @movie.update_attributes!(movie_params)  # new way  
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
 
 def destroy
   @movie = Movie.find(params[:id])
